ccea92db5458b2bde720f6a1a968a79360821388
Dmitry Zinkin <yashtlsht@mail.ru>
2022-09-27 13:04:19 +0300

    commit

--- src/main/java/com/example/crud_boot_security_proj/service/UserServiceImpl.java
+++ src/main/java/com/example/crud_boot_security_proj/service/UserServiceImpl.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService{
-    private final UserDao userDao;
+    private final UserDao userDao;//Для commit
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
